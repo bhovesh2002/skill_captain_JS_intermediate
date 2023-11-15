7fd4a009289729d90d6c5693da11e5f7b0d4c1f2
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2023-11-15 18:19:35 +0530

    Create ES6Replace.js

---
+++ Day1/ES6Replace.js
@@ -0,0 +1,5 @@
+let data = "JavaScript is a programming language used to create dynamic content for websites. It achieves this by adding new HTML elements while modifying existing ones. Many coders enhance web development skills using JavaScript to create user-friendly and interactive websites.";
+
+let answer = data.replace(/javascript/gi,"ES6");
+
+console.log(answer);
